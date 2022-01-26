5d621617aa1694d773b396aea5b1e4ac1c502844
Omid Soroush <61864572+OmidSoroush@users.noreply.github.com>
2022-01-26 14:41:01 +0100

    HBase Table Creation

--- HadoopMaterials/HBaseExamples.py
+++ HadoopMaterials/HBaseExamples.py
@@ -11,7 +11,7 @@ if (ratings.exists()):
 ratings.create('rating')
 
 print("Parsing the ml-100k ratings data...\n")
-ratingFile = open("e:/Downloads/ml-100k/ml-100k/u.data", "r")
+ratingFile = open("/Users/omidsoroush/Downloads/ml-100k/u.data", "r")
 
 batch = ratings.batch()
 
